4a02c7b966b3d64f46c891322db7ae5ca23f6880
Evan <hyflying888@163.com>
2025-01-10 23:41:45 -0800

    Finish Hello, World

--- hello_test.go
+++ hello_test.go
@@ -4,16 +4,30 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	// use subtests
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
+	// t.Run("saying hello to people", func(t *testing.T) {
+	// 	got := Hello("Chris")
+	// 	want := "Hello, Chris"
+	// 	if got != want {
+	// 		assertCorrectMessage(t, got, want)
+	// 	}
+	// })
+	// t.Run(("say 'Hello, world' when an empty string is supplied"), func(t *testing.T) {
+	// 	got := Hello("")
+	// 	want := "Hello, world"
+	// 	if got != want {
+	// 		assertCorrectMessage(t, got, want)
+	// 	}
+	// })
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		if got != want {
 			assertCorrectMessage(t, got, want)
 		}
 	})
-	t.Run(("say 'Hello, world' when an empty string is supplied"), func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, world"
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
 		if got != want {
 			assertCorrectMessage(t, got, want)
 		}
